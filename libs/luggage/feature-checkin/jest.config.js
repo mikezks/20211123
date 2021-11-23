cd2c6bee44c1d91b998afc45ee61bbcba2c3acb5
michael.zikes <michael.zikes@intauria.com>
2021-11-23 16:06:46 +0100

    DDD

---
+++ libs/luggage/feature-checkin/jest.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  displayName: 'luggage-feature-checkin',
+  preset: '../../../jest.preset.js',
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+    },
+  },
+  coverageDirectory: '../../../coverage/libs/luggage/feature-checkin',
+  transform: {
+    '^.+\\.(ts|js|html)$': 'jest-preset-angular',
+  },
+  snapshotSerializers: [
+    'jest-preset-angular/build/serializers/no-ng-attributes',
+    'jest-preset-angular/build/serializers/ng-snapshot',
+    'jest-preset-angular/build/serializers/html-comment',
+  ],
+};
